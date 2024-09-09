bc719e3e0ed91d85f8556260ffcb9955e3c3a823
Pavan BG <gpbabc@yahoo.com>
2024-09-09 22:37:44 +0530

    Updated benchmark to use both sym and asym

--- benchmark/test_jac_benchmark.py
+++ benchmark/test_jac_benchmark.py
@@ -5,6 +5,7 @@ import time
 
 from examples.advection_diffusion import AdvectionDiffusion
 from splitfxm.domain import Domain
+from splitfxm.derivatives import FDSchemes
 from splitfxm.simulation import Simulation
 from splitfxm.schemes import default_scheme
 
@@ -35,12 +36,68 @@ def test_dense_sparse_jac_comparison():
     # Construct initial vector
     x0 = d.listify_interior(split, split_loc)
 
+    # Some output
+    print('\n' + '*' * 10)
+    print('SYMMETRIC STENCIL')
+    print('*' * 10)
+    print('Computing...')
+
     # Construct dense Jacobian
     def _f(u): return s.get_residuals_from_list(u, split, split_loc)
     start_time = time.perf_counter()
     jac_dense = nd.Jacobian(_f, method='forward', step=1e-8)(x0)
     dense_time = time.perf_counter() - start_time
-    print(f'\nDense Jacobian took {dense_time} s')
+    print(f'Dense Jacobian took {dense_time} s')
+
+    # Construct sparse Jacobian
+    start_time = time.perf_counter()
+    jac_sparse = s.jacobian(x0, split, split_loc)
+    sparse_time = time.perf_counter() - start_time
+    print(f'Sparse Jacobian took {sparse_time} s')
+
+    # Show timing results in prompt in pytest
+    assert sparse_time < dense_time
+    assert np.allclose(jac_sparse.toarray(), jac_dense, atol=1e-7)
+
+
+def test_dense_sparse_jac_comparison_asym():
+    method = 'FDM'
+    m = AdvectionDiffusion(c=0.2, nu=0.001, method=method)
+    d = Domain.from_size(250, 1, 2, ["u", "v", "w"])
+    ics = {"u": "gaussian", "v": "rarefaction"}
+    bcs = {
+        "u": {
+            "left": "periodic",
+            "right": "periodic"
+        },
+        "v": {
+            "left": {"dirichlet": 3},
+            "right": {"dirichlet": 4}
+        },
+        "w": {
+            "left": {"dirichlet": 2},
+            "right": "periodic"
+        }
+    }
+    s = Simulation(d, m, ics, bcs, FDSchemes.RIGHT_BIAS)
+    split = True
+    split_loc = 1
+
+    # Construct initial vector
+    x0 = d.listify_interior(split, split_loc)
+
+    # Some output
+    print('\n' + '*' * 10)
+    print('ASYMMETRIC STENCIL')
+    print('*' * 10)
+    print('Computing...')
+
+    # Construct dense Jacobian
+    def _f(u): return s.get_residuals_from_list(u, split, split_loc)
+    start_time = time.perf_counter()
+    jac_dense = nd.Jacobian(_f, method='forward', step=1e-8)(x0)
+    dense_time = time.perf_counter() - start_time
+    print(f'Dense Jacobian took {dense_time} s')
 
     # Construct sparse Jacobian
     start_time = time.perf_counter()
