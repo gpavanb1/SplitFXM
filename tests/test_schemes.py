ce7096367886c4b41cc399edbc35146e2b231d75
Pavan BG <gpbabc@yahoo.com>
2024-09-12 23:15:03 +0530

    Added schemes tests

---
+++ tests/test_schemes.py
@@ -0,0 +1,28 @@
+import pytest
+
+from splitfxm.error import SFXM
+from splitfxm.schemes import default_scheme, FVSchemes, FDSchemes
+
+
+def test_default_scheme_fvm():
+    """
+    Test default_scheme with 'FVM' method.
+    """
+    assert default_scheme(
+        "FVM") == FVSchemes.LF, "default_scheme('FVM') should return FVSchemes.LF"
+
+
+def test_default_scheme_fdm():
+    """
+    Test default_scheme with 'FDM' method.
+    """
+    assert default_scheme(
+        "FDM") == FDSchemes.CENTRAL, "default_scheme('FDM') should return FDSchemes.CENTRAL"
+
+
+def test_default_scheme_invalid_method():
+    """
+    Test default_scheme with an invalid method.
+    """
+    with pytest.raises(SFXM, match="Invalid numerical method specified"):
+        default_scheme("InvalidMethod")
