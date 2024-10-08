0c3e02288608dbc3712771d38a41887c0b7e44fa
Pavan BG <gpbabc@yahoo.com>
2024-10-09 00:12:23 +0530

    Optimized FDM

--- splitfxm/derivatives.pyx
+++ splitfxm/derivatives.pyx
@@ -1,12 +1,17 @@
+import cython
+import numpy as np
 from .error import SFXM
 from .schemes import stencil_sizes
 cimport numpy as np
+from libc.math cimport pow
 
 # Function for handling callable functions
 # Enum is not used as it is not supported by Cython
+@cython.boundscheck(False)
+@cython.wraparound(False)
 cdef np.ndarray derivative_callable(
-    F,  # Function pointer type for callable functions
-    list cell_sub,  # List of Cell, the stencil points
+    double[:, :] F,  # Function pointer type for callable functions
+    double[:] cell_sub_x,  # List of Cell, the stencil points
     int scheme,  # FDSchemes.value: the finite difference scheme to use
     int stencil_size,  # Size of the stencil
     int order=1,  # Order of the derivative (1 for first, 2 for second)
@@ -16,10 +21,10 @@ cdef np.ndarray derivative_callable(
 
     Parameters
     ----------
-    F : callable
-        A function that takes the values at the stencil points.
-    cell_sub : list of Cell
-        The stencil, a list of cells around the point of interest.
+    F_values : double[:, :]
+        Precomputed function values at the stencil points (evaluated at corresponding points).
+    cell_sub_x : double[:]
+        The x-coordinates of the stencil points.
     scheme : FDSchemes.value
         The finite difference scheme to use (1 or 2).
     stencil_size : int
@@ -37,40 +42,68 @@ cdef np.ndarray derivative_callable(
     SFXM
         If an improper stencil size is provided or an unsupported scheme is used.
     """
-    cdef np.ndarray Fl, Fr, Dl, Dr, Dw, De
+    cdef double[:] Fll, Fl, Fc, Fr, Fw, Fe, Fans
     cdef double dx, dx_w, dx_e
+    cdef int i, n
 
-    if len(cell_sub) != stencil_size:
-        raise SFXM(f"Improper stencil size. Expected {stencil_size}, got {len(cell_sub)}")
+    # Initialize memoryviews
+    n = F.shape[1]
+    Fw = memoryview(np.zeros(n, dtype=np.double))
+    Fe = memoryview(np.zeros(n, dtype=np.double))
+    Fans = memoryview(np.zeros(n, dtype=np.double))
+
+    if len(cell_sub_x) != stencil_size:
+        raise SFXM(f"Improper stencil size. Expected {stencil_size}, got {len(cell_sub_x)}")
 
     if order == 1:
         if scheme == 1:  # FDSchemes.CENTRAL
-            Fl = F(cell_sub[0].values())
-            Fr = F(cell_sub[2].values())
-            dx = cell_sub[2].x() - cell_sub[0].x()
-            return (Fr - Fl) / dx
+            Fl = F[0, :]
+            Fr = F[2, :]
+            dx = cell_sub_x[2] - cell_sub_x[0]
+
+            for i in range(n):
+                Fans[i] = (Fr[i] - Fl[i]) / dx
+            return np.asarray(Fans)
         elif scheme == 2:
-            return (-F(cell_sub[0].values()) - F(cell_sub[1].values()) + 
-                    F(cell_sub[2].values()) + F(cell_sub[3].values())) / (4 * (cell_sub[1].x() - cell_sub[0].x()))
+            Fll = F[0, :]
+            Fl = F[1, :]
+            Fc = F[2, :]
+            Fr = F[3, :]
+
+            for i in range(n):
+                Fans[i] = (-Fll[i] - Fl[i] + Fc[i] + Fr[i]) / (4 * (cell_sub_x[1] - cell_sub_x[0]))
+            return np.asarray(Fans)
         else:
             raise SFXM("Unsupported scheme")
 
     elif order == 2:
         if scheme == 1:  # FDSchemes.CENTRAL
-            Dl = F(cell_sub[0].values())
-            Dr = F(cell_sub[1].values())
-            dx_w = cell_sub[1].x() - cell_sub[0].x()
-            Dw = (Dr - Dl) / dx_w
+            Fl = F[0, :]
+            Fr = F[1, :]
+            dx_w = cell_sub_x[1] - cell_sub_x[0]
 
-            Dl = F(cell_sub[1].values())
-            Dr = F(cell_sub[2].values())
-            dx_e = cell_sub[2].x() - cell_sub[1].x()
-            De = (Dr - Dl) / dx_e
+            for i in range(n):
+                Fw[i] = (Fr[i] - Fl[i]) / dx_w
 
-            return (De - Dw) / ((dx_w + dx_e) / 2)
+            Fl = F[1, :]
+            Fr = F[2, :]
+            dx_e = cell_sub_x[2] - cell_sub_x[1]
+            
+            for i in range(n):
+                Fe[i] = (Fr[i] - Fl[i]) / dx_e
+
+            for i in range(n):
+                Fans[i] = (Fe[i] - Fw[i]) / ((dx_w + dx_e) / 2)
+            return np.asarray(Fans)
         elif scheme == 2:
-            return (F(cell_sub[0].values()) - F(cell_sub[1].values()) - 
-                    F(cell_sub[2].values()) + F(cell_sub[3].values())) / (2 * (cell_sub[1].x() - cell_sub[0].x())**2)
+            Fll = F[0, :]
+            Fl = F[1, :]
+            Fc = F[2, :]
+            Fr = F[3, :]
+
+            for i in range(n):
+                Fans[i] = (Fll[i] - Fl[i] - Fc[i] + Fr[i]) / (2 * (cell_sub_x[1] - cell_sub_x[0])**2)
+            return np.asarray(Fans)
         else:
             raise SFXM("Unsupported scheme")
     else:
@@ -78,67 +111,68 @@ cdef np.ndarray derivative_callable(
 
 # Function for handling precomputed values
 cdef double derivative_values(
-    np.ndarray values,  # Precomputed values of the function at the stencil points
-    list cell_sub,  # List of Cell, the stencil points
+    double[:] values,  # Memoryview for precomputed values at stencil points
+    double[:] cell_sub_x,  # Memoryview for cell positions (x-coordinates)
     int scheme,  # FDSchemes: the finite difference scheme to use
-    int stencil_size, # Size of the stencil
+    int stencil_size,  # Size of the stencil
     int order=1  # Order of the derivative (1 for first, 2 for second)
 ):
     """
-    Calculate the derivative using precomputed values.
-
+    Calculate the derivative using precomputed values and cell positions.
+    
     Parameters
     ----------
-    values : np.ndarray
+    values : double[:]
         Precomputed values of the function at the stencil points.
-    cell_sub : list of Cell
-        The stencil, a list of cells around the point of interest.
-    scheme : FDSchemes
+    cell_sub_x : double[:]
+        The x-coordinates of the stencil points.
+    scheme : int
         The finite difference scheme to use (CENTRAL or RIGHT_BIAS).
     order : int, optional
         The order of the derivative (1 for first derivative, 2 for second derivative). Default is 1.
-
+        
     Returns
     -------
-    np.ndarray
+    double
         The computed derivative based on the selected scheme and order.
-
+    
     Raises
     ------
-    SFXM
+    ValueError
         If an improper stencil size is provided or an unsupported scheme is used.
     """
-    cdef double dx
-
-    if len(cell_sub) != stencil_size:
-        raise SFXM(f"Improper stencil size. Expected {stencil_size}, got {len(cell_sub)}")
+    cdef double dx, Dw, De
+    
+    if stencil_size != values.shape[0] or stencil_size != cell_sub_x.shape[0]:
+        raise SFXM(f"Improper stencil size. Expected {stencil_size}, got {values.shape[0]}")
 
     if order == 1:
         if scheme == 1:  # FDSchemes.CENTRAL
-            dx = cell_sub[2].x() - cell_sub[0].x()
+            dx = cell_sub_x[2] - cell_sub_x[0]
             return (values[2] - values[0]) / dx
         elif scheme == 2:  # FDSchemes.RIGHT_BIAS
-            dx = cell_sub[1].x() - cell_sub[0].x()
+            dx = cell_sub_x[1] - cell_sub_x[0]
             return (-values[0] - values[1] + values[2] + values[3]) / (4 * dx)
         else:
             raise SFXM("Unsupported scheme")
 
     elif order == 2:
         if scheme == 1:  # FDSchemes.CENTRAL
-            dx_w = cell_sub[1].x() - cell_sub[0].x()
+            dx_w = cell_sub_x[1] - cell_sub_x[0]
             Dw = (values[1] - values[0]) / dx_w
 
-            dx_e = cell_sub[2].x() - cell_sub[1].x()
+            dx_e = cell_sub_x[2] - cell_sub_x[1]
             De = (values[2] - values[1]) / dx_e
 
             return (De - Dw) / ((dx_w + dx_e) / 2)
         elif scheme == 2:  # FDSchemes.RIGHT_BIAS
-            dx = cell_sub[1].x() - cell_sub[0].x()
-            return (values[0] - values[1] - values[2] + values[3]) / (2 * dx**2)
+            dx = cell_sub_x[1] - cell_sub_x[0]
+            return (values[0] - values[1] - values[2] + values[3]) / (2 * pow(dx, 2))
         else:
             raise SFXM("Unsupported scheme")
     else:
         raise SFXM("Unsupported scheme or order")
+
 
 # First derivative functions
 def Dx(F, cell_sub, scheme):
@@ -159,7 +193,9 @@ def Dx(F, cell_sub, scheme):
     numpy.ndarray
         The computed first derivative based on the selected scheme.
     """
-    return derivative_callable(F, cell_sub, scheme.value, stencil_sizes[scheme], order=1)
+    cell_sub_x = memoryview(np.array([cell.x() for cell in cell_sub], dtype=np.double))
+    F_values = memoryview(np.array([F(cell.values()) for cell in cell_sub], dtype=np.double))
+    return derivative_callable(F_values, cell_sub_x, scheme.value, stencil_sizes[scheme], order=1)
 
 def dx(values, cell_sub, scheme):
     """
@@ -179,7 +215,8 @@ def dx(values, cell_sub, scheme):
     numpy.ndarray
         The computed first derivative based on the selected scheme.
     """
-    return derivative_values(values, cell_sub, scheme.value, stencil_sizes[scheme], order=1)
+    cell_sub_x = memoryview(np.array([cell.x() for cell in cell_sub], dtype=np.double))
+    return derivative_values(values, cell_sub_x, scheme.value, stencil_sizes[scheme], order=1)
 
 # Second derivative functions
 def D2x(D, cell_sub, scheme):
@@ -200,7 +237,9 @@ def D2x(D, cell_sub, scheme):
     numpy.ndarray
         The computed second derivative based on the selected scheme.
     """
-    return derivative_callable(D, cell_sub, scheme.value, stencil_sizes[scheme], order=2)
+    cell_sub_x = memoryview(np.array([cell.x() for cell in cell_sub], dtype=np.double))
+    D_values = memoryview(np.array([D(cell.values()) for cell in cell_sub], dtype=np.double))
+    return derivative_callable(D_values, cell_sub_x, scheme.value, stencil_sizes[scheme], order=2)
 
 def d2x(values, cell_sub, scheme):
     """
@@ -220,4 +259,5 @@ def d2x(values, cell_sub, scheme):
     numpy.ndarray
         The computed second derivative based on the selected scheme.
     """
-    return derivative_values(values, cell_sub, scheme.value, stencil_sizes[scheme], order=2)
+    cell_sub_x = memoryview(np.array([cell.x() for cell in cell_sub], dtype=np.double))
+    return derivative_values(values, cell_sub_x, scheme.value, stencil_sizes[scheme], order=2)
--- tests/test_derivative.py
+++ tests/test_derivative.py
@@ -7,35 +7,42 @@ from splitfxm.schemes import FDSchemes
 from splitfxm.error import SFXM
 
 
+def array(*args, **kwargs):
+    # Overriding np.array dtype locally
+    """Wrapper for np.array to default to float64."""
+    kwargs.setdefault('dtype', np.float64)  # Set default dtype to float64
+    return np.array(*args, **kwargs)
+
+
 def test_Dx_central():
     # Test first derivative with CENTRAL scheme
-    cell_sub = [Cell(0, np.array([1.])), Cell(
-        1, np.array([2.])), Cell(2, np.array([3.]))]
+    cell_sub = [Cell(0, array([1.])), Cell(
+        1, array([2.])), Cell(2, array([3.]))]
     result = Dx(lambda v: v, cell_sub, FDSchemes.CENTRAL)
-    assert np.allclose(result, np.array([1.]))
+    assert np.allclose(result, array([1.]))
 
 
 def test_Dx_right_bias():
     # Test first derivative with RIGHT_BIAS scheme
-    cell_sub = [Cell(0, np.array([1.])), Cell(1, np.array([2.])),
-                Cell(2, np.array([3.])), Cell(3, np.array([4.]))]
+    cell_sub = [Cell(0, array([1.])), Cell(1, array([2.])),
+                Cell(2, array([3.])), Cell(3, array([4.]))]
     result = Dx(lambda v: v, cell_sub,
                 FDSchemes.RIGHT_BIAS)
-    assert np.allclose(result, np.array([1.]))
+    assert np.allclose(result, array([1.]))
 
 
 def test_D2x_central():
     # Test second derivative with CENTRAL scheme
-    cell_sub = [Cell(0, np.array([1.])), Cell(
-        1, np.array([2.])), Cell(2, np.array([3.]))]
+    cell_sub = [Cell(0, array([1.])), Cell(
+        1, array([2.])), Cell(2, array([3.]))]
     result = D2x(lambda v: v, cell_sub, FDSchemes.CENTRAL)
     assert result == 0.0
 
 
 def test_D2x_right_bias():
     # Test second derivative with RIGHT_BIAS scheme
-    cell_sub = [Cell(0, np.array([1])), Cell(1, np.array([2])),
-                Cell(2, np.array([3])), Cell(3, np.array([4]))]
+    cell_sub = [Cell(0, array([1])), Cell(1, array([2])),
+                Cell(2, array([3])), Cell(3, array([4]))]
     result = D2x(lambda v: v, cell_sub,
                  FDSchemes.RIGHT_BIAS)
     assert result == 0.0
@@ -43,42 +50,42 @@ def test_D2x_right_bias():
 
 def test_dx_first_central():
     # Test dx for precomputed values with CENTRAL scheme
-    cell_sub = [Cell(0, np.array([1])), Cell(
-        1, np.array([2])), Cell(2, np.array([3]))]
-    values = np.array([1, 2, 3])
+    cell_sub = [Cell(0, array([1])), Cell(
+        1, array([2])), Cell(2, array([3]))]
+    values = array([1, 2, 3])
     result = dx(values, cell_sub, FDSchemes.CENTRAL)
     assert result == 1.
 
 
 def test_dx_first_right_bias():
     # Test dx for precomputed values with RIGHT_BIAS scheme
-    cell_sub = [Cell(0, np.array([1])), Cell(1, np.array([2])),
-                Cell(2, np.array([3])), Cell(3, np.array([4]))]
-    values = np.array([1, 2, 3, 4])
+    cell_sub = [Cell(0, array([1])), Cell(1, array([2])),
+                Cell(2, array([3])), Cell(3, array([4]))]
+    values = array([1, 2, 3, 4])
     result = dx(values, cell_sub, FDSchemes.RIGHT_BIAS)
     assert result == 1.
 
 
 def test_d2x_second_central():
     # Test d2x for precomputed values with CENTRAL scheme
-    cell_sub = [Cell(0, np.array([1])), Cell(
-        1, np.array([2])), Cell(2, np.array([3]))]
-    values = np.array([1, 2, 3])
+    cell_sub = [Cell(0, array([1])), Cell(
+        1, array([2])), Cell(2, array([3]))]
+    values = array([1, 2, 3])
     result = d2x(values, cell_sub, FDSchemes.CENTRAL)
     assert result == 0.
 
 
 def test_d2x_second_right_bias():
     # Test d2x for precomputed values with RIGHT_BIAS scheme
-    cell_sub = [Cell(0, np.array([1])), Cell(1, np.array([2])),
-                Cell(2, np.array([3])), Cell(3, np.array([4]))]
-    values = np.array([1, 2, 3, 4])
+    cell_sub = [Cell(0, array([1])), Cell(1, array([2])),
+                Cell(2, array([3])), Cell(3, array([4]))]
+    values = array([1, 2, 3, 4])
     result = d2x(values, cell_sub, FDSchemes.RIGHT_BIAS)
     assert result == 0.
 
 
 def test_improper_stencil_size():
     # Test for improper stencil size that raises SFXM
-    cell_sub = [Cell(0, np.array([1])), Cell(1, np.array([2]))]
+    cell_sub = [Cell(0, array([1])), Cell(1, array([2]))]
     with pytest.raises(SFXM):
         Dx(lambda v: v, cell_sub, FDSchemes.CENTRAL)
